dbc49c60305344bc7714968f169233470d3eccee
Johannes Haseitl <derhaside@gmail.com>
2014-12-26 16:42:46 +0100

    Create Plugin.php

---
+++ src/Plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace derhasi\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface {
+    public function activate(Composer $composer, IOInterface $io) {
+        
+    }
+}
