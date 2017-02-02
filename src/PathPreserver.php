e6c1c0bf10a212ef856dac079133989078318ede
Demin Yin <deminy@deminy.net>
2017-02-02 15:31:02 -0800

    refactors and clean up before updating the implementation

--- src/PathPreserver.php
+++ src/PathPreserver.php
@@ -1,271 +1,284 @@
 <?php
 
 /**
- * Contains \derhasi\Composer\PathPreserver
+ * Contains \deminy\Composer\PathPreserver
  */
 
-namespace derhasi\Composer;
+namespace deminy\Composer;
+
+use Composer\Util\FileSystem;
+use Composer\IO\IOInterface;
 
 /**
  * Class PathPreserver
  */
-class PathPreserver {
+class PathPreserver
+{
 
-  /**
-   * Temporary file permission to allow moving protected paths.
-   */
-  const FILEPERM = 0755;
+    /**
+     * Temporary file permission to allow moving protected paths.
+     */
+    const FILEPERM = 0755;
 
-  /**
-   * @var string
-   */
-  protected $cacheDir;
+    /**
+     * @var string
+     */
+    protected $cacheDir;
 
-  /**
-   * @var string[]
-   */
-  protected $installPaths;
+    /**
+     * @var string[]
+     */
+    protected $installPaths;
 
-  /**
-   * @var string[]
-   */
-  protected $preservePaths;
+    /**
+     * @var string[]
+     */
+    protected $preservePaths;
 
-  /**
-   * @var \Composer\Util\FileSystem
-   */
-  protected $filesystem;
+    /**
+     * @var \Composer\Util\FileSystem
+     */
+    protected $filesystem;
 
-  /**
-   * @var \Composer\IO\IOInterface
-   */
-  protected $io;
+    /**
+     * @var \Composer\IO\IOInterface
+     */
+    protected $io;
 
-  /**
-   * @var string[string]
-   */
-  protected $backups = array();
+    /**
+     * @var string[string]
+     */
+    protected $backups = array();
 
-  /**
-   * @var string[string]
-   */
-  protected $filepermissions = array();
+    /**
+     * @var string[string]
+     */
+    protected $filepermissions = array();
 
-  /**
-   * Constructor.
-   *
-   * @param string[] $installPaths
-   *   Array of install paths (must be absolute)
-   * @param string[] $preservePaths
-   *   Array of preservable paths (must be absolute)
-   * @param string $cacheDir
-   *   Absolute path to composer cache dir.
-   * @param \Composer\Util\FileSystem $filesystem
-   *   The filesystem provided by composer to work with.
-   * @param \Composer\IO\IOInterface $io
-   *   IO interface for writing messages.
-   */
-  public function __construct($installPaths, $preservePaths, $cacheDir, \Composer\Util\FileSystem $filesystem, \Composer\IO\IOInterface $io) {
-    $this->installPaths = array_unique($installPaths);
-    $this->preservePaths = array_unique($preservePaths);
-    $this->filesystem = $filesystem;
-    $this->cacheDir = $cacheDir;
-    $this->io = $io;
-  }
-
-  /**
-   * Backs up the paths.
-   */
-  public function preserve() {
-
-    foreach ($this->installPaths as $installPath) {
-      $installPathNormalized = $this->filesystem->normalizePath($installPath);
-
-      // Check if any path may be affected by modifying the install path.
-      $relevant_paths = array();
-      foreach ($this->preservePaths as $path) {
-        $normalizedPath = $this->filesystem->normalizePath($path);
-        if (static::file_exists($path) && strpos($normalizedPath, $installPathNormalized) === 0) {
-          $relevant_paths[] = $normalizedPath;
-        }
-      }
-
-      // If no paths need to be backed up, we simply proceed.
-      if (empty($relevant_paths)) {
-        continue;
-      }
-
-      $unique = $installPath . ' ' . time();
-      $cache_root = $this->filesystem->normalizePath($this->cacheDir . '/preserve-paths/' . sha1($unique));
-      $this->filesystem->ensureDirectoryExists($cache_root);
-
-      // Before we back paths up, we need to make sure, permissions are
-      // sufficient to that task.
-      $this->preparePathPermissions($relevant_paths);
-
-      foreach ($relevant_paths as $original) {
-        $backup_location = $cache_root . '/' . sha1($original);
-        $this->filesystem->rename($original, $backup_location);
-        $this->backups[$original] = $backup_location;
-      }
-    }
-  }
-
-  /**
-   * Restore previously backed up paths.
-   *
-   * @see PathPreserver::backupSubpaths()
-   */
-  public function rollback() {
-    if (empty($this->backups)) {
-      return;
+    /**
+     * Constructor.
+     *
+     * @param string[] $installPaths
+     *   Array of install paths (must be absolute)
+     * @param string[] $preservePaths
+     *   Array of preservable paths (must be absolute)
+     * @param string $cacheDir
+     *   Absolute path to composer cache dir.
+     * @param \Composer\Util\FileSystem $filesystem
+     *   The filesystem provided by composer to work with.
+     * @param \Composer\IO\IOInterface $io
+     *   IO interface for writing messages.
+     */
+    public function __construct(
+        array $installPaths,
+        array $preservePaths,
+        $cacheDir,
+        FileSystem $filesystem,
+        IOInterface $io
+    ) {
+        $this->installPaths  = array_unique($installPaths);
+        $this->preservePaths = array_unique($preservePaths);
+        $this->cacheDir      = $cacheDir;
+        $this->filesystem    = $filesystem;
+        $this->io            = $io;
     }
 
-    foreach ($this->backups as $original => $backup_location) {
+    /**
+     * Backs up the paths.
+     */
+    public function preserve() 
+    {
+        foreach ($this->installPaths as $installPath) {
+            $installPathNormalized = $this->filesystem->normalizePath($installPath);
 
-      // Remove any code that was placed by the package at the place of
-      // the original path.
-      if (static::file_exists($original)) {
-        if (is_dir($original)) {
-          $this->filesystem->emptyDirectory($original, false);
-          $this->filesystem->removeDirectory($original);
+            // Check if any path may be affected by modifying the install path.
+            $relevant_paths = array();
+            foreach ($this->preservePaths as $path) {
+                $normalizedPath = $this->filesystem->normalizePath($path);
+                if ($this->fileExists($path) && strpos($normalizedPath, $installPathNormalized) === 0) {
+                    $relevant_paths[] = $normalizedPath;
+                }
+            }
+
+            // If no paths need to be backed up, we simply proceed.
+            if (empty($relevant_paths)) {
+                continue;
+            }
+
+            $unique = $installPath . ' ' . time();
+            $cache_root = $this->filesystem->normalizePath($this->cacheDir . '/preserve-paths/' . sha1($unique));
+            $this->filesystem->ensureDirectoryExists($cache_root);
+
+            // Before we back paths up, we need to make sure, permissions are
+            // sufficient to that task.
+            $this->preparePathPermissions($relevant_paths);
+
+            foreach ($relevant_paths as $original) {
+                $backup_location = $cache_root . '/' . sha1($original);
+                $this->filesystem->rename($original, $backup_location);
+                $this->backups[$original] = $backup_location;
+            }
         }
-        else {
-          $this->filesystem->remove($original);
+    }
+
+    /**
+     * Restore previously backed up paths.
+     *
+     * @see PathPreserver::backupSubpaths()
+     */
+    public function rollback() 
+    {
+        if (empty($this->backups)) {
+            return;
         }
 
-        $this->io->write(sprintf('<comment>Files of installed package were overwritten with preserved path %s!</comment>', $original), true);
-      }
+        foreach ($this->backups as $original => $backup_location) {
+            // Remove any code that was placed by the package at the place of
+            // the original path.
+            if ($this->fileExists($original)) {
+                if (is_dir($original)) {
+                    $this->filesystem->emptyDirectory($original, false);
+                    $this->filesystem->removeDirectory($original);
+                } else {
+                    $this->filesystem->remove($original);
+                }
 
-      $folder = dirname($original);
-      $this->filesystem->ensureDirectoryExists($folder);
-      // Make sure we can write the file to the folder.
-      $this->makePathWritable($folder);
-      $this->filesystem->rename($backup_location, $original);
+                $this->io->write(
+                    sprintf(
+                        '<comment>Files of installed package were overwritten with preserved path %s!</comment>',
+                        $original
+                    ),
+                    true
+                );
+            }
 
-      if ($this->filesystem->isDirEmpty(dirname($backup_location))) {
-        $this->filesystem->removeDirectory(dirname($backup_location));
-      }
+            $folder = dirname($original);
+            $this->filesystem->ensureDirectoryExists($folder);
+            // Make sure we can write the file to the folder.
+            $this->makePathWritable($folder);
+            $this->filesystem->rename($backup_location, $original);
+
+            if ($this->filesystem->isDirEmpty(dirname($backup_location))) {
+                $this->filesystem->removeDirectory(dirname($backup_location));
+            }
+        }
+
+        // Restore all path permissions, that where set for the sake of moving
+        // things around.
+        $this->restorePathPermissions();
+
+        // With a clean array, we can start over.
+        $this->backups = array();
     }
 
-    // Restore all path permissions, that where set for the sake of moving
-    // things around.
-    $this->restorePathPermissions();
+    /**
+     * Prepares source paths for backup.
+     *
+     * @param $paths
+     *
+     * @see PathPreserver::restorePathPermissions()
+     */
+    protected function preparePathPermissions($paths) 
+    {
+        foreach ($paths as $path) {
+            // In the case the path or its parent is not writable, we cannot move the
+            // path. Therefore we change the permissions temporarily and restore them
+            // later.
+            if (!is_writable($path)) {
+                $this->makePathWritable($path);
+            }
 
-    // With a clean array, we can start over.
-    $this->backups = array();
-  }
-
-  /**
-   * Prepares source paths for backup.
-   *
-   * @param $paths
-   *
-   * @see PathPreserver::restorePathPermissions()
-   */
-  protected function preparePathPermissions($paths) {
-    foreach ($paths as $path) {
-      // In the case the path or its parent is not writable, we cannot move the
-      // path. Therefore we change the permissions temporarily and restore them
-      // later.
-      if (!is_writable($path)) {
-        $this->makePathWritable($path);
-      }
-
-      $parent = dirname($path);
-      if (!is_writable($parent)) {
-        $this->makePathWritable($parent);
-      }
-    }
-  }
-
-  /**
-   * Helper to make path writable.
-   *
-   * @param string $path
-   */
-  protected function makePathWritable($path) {
-    // Make parent writable, before we can change the path itself.
-    $parent = dirname($path);
-    if ($parent != '.' && !is_writable($parent)) {
-      $this->makePathWritable($parent);
+            $parent = dirname($path);
+            if (!is_writable($parent)) {
+                $this->makePathWritable($parent);
+            }
+        }
     }
 
-    $this->filepermissions[$path] = fileperms($path);
-    chmod($path, static::FILEPERM);
-  }
+    /**
+     * Helper to make path writable.
+     *
+     * @param string $path
+     */
+    protected function makePathWritable($path) 
+    {
+        // Make parent writable, before we can change the path itself.
+        $parent = dirname($path);
+        if ($parent != '.' && !is_writable($parent)) {
+            $this->makePathWritable($parent);
+        }
 
-  /**
-   * Restores path permissions that have been changed before.
-   *
-   * @see PathPreserver::preparePathPermissions()
-   */
-  protected function restorePathPermissions() {
-    // We restore child permissions first.
-    arsort($this->filepermissions);
-
-    foreach ($this->filepermissions as $path => $perm) {
-      chmod($path, $perm);
-    }
-  }
-
-  /**
-   * Check if file really exists.
-   *
-   * As php can only determine, whether a file or folder exists when the parent
-   * directory is executable, we need to provide a workaround.
-   *
-   * @param $path
-   *   The path as in file_exists()
-   *
-   * @return bool
-   *   Returns TRUE if file exists, like in file_exists(),
-   *   but without restriction.
-   *
-   * @see file_exists()
-   */
-  static public function file_exists($path) {
-
-    // Get all parent directories.
-    $folders = array();
-    $reset_perms = array();
-    $folder = $path;
-    while ($folder = dirname($folder)) {
-      if ($folder === '.' || $folder === '/' || preg_match("/^.:\\\\$/", $folder)) {
-        break;
-      }
-      elseif ($folder === '') {
-        continue;
-      }
-      $folders[] = $folder;
+        $this->filepermissions[$path] = fileperms($path);
+        chmod($path, static::FILEPERM);
     }
 
-    foreach (array_reverse($folders) as $current_folder) {
-      // In the case a parent folder does not exist, the file cannot exist.
-      if (!is_dir($current_folder)) {
-        $return = FALSE;
-        break;
-      }
-      // In the case the folder is really a folder, but not executable, we need
-      // to change that, so we can check if the file really exists.
-      elseif (!is_executable($current_folder)) {
-        $reset_perms[$current_folder] = fileperms($current_folder);
-        chmod($current_folder, 0755);
-      }
+    /**
+     * Restores path permissions that have been changed before.
+     *
+     * @see PathPreserver::preparePathPermissions()
+     */
+    protected function restorePathPermissions() 
+    {
+        // We restore child permissions first.
+        arsort($this->filepermissions);
+
+        foreach ($this->filepermissions as $path => $perm) {
+            chmod($path, $perm);
+        }
     }
 
-    if (!isset($return)) {
-      $return = file_exists($path);
+    /**
+     * Check if file really exists.
+     *
+     * As php can only determine, whether a file or folder exists when the parent
+     * directory is executable, we need to provide a workaround.
+     *
+     * @param $path
+     *   The path as in file_exists()
+     *
+     * @return bool
+     *   Returns TRUE if file exists, like in file_exists(),
+     *   but without restriction.
+     *
+     * @see file_exists()
+     */
+    protected function fileExists($path)
+    {
+        // Get all parent directories.
+        $folders = array();
+        $reset_perms = array();
+        $folder = $path;
+        while ($folder = dirname($folder)) {
+            if ($folder === '.' || $folder === '/' || preg_match("/^.:\\\\$/", $folder)) {
+                break;
+            } elseif ($folder === '') {
+                continue;
+            }
+            $folders[] = $folder;
+        }
+
+        foreach (array_reverse($folders) as $current_folder) {
+            // In the case a parent folder does not exist, the file cannot exist.
+            if (!is_dir($current_folder)) {
+                $return = false;
+                break;
+            } elseif (!is_executable($current_folder)) {
+                // In the case the folder is really a folder, but not executable, we need
+                // to change that, so we can check if the file really exists.
+                $reset_perms[$current_folder] = fileperms($current_folder);
+                chmod($current_folder, 0755);
+            }
+        }
+
+        if (!isset($return)) {
+            $return = file_exists($path);
+        }
+
+        // Reset permissions in reverse order.
+        foreach (array_reverse($reset_perms, true) as $folder => $mode) {
+            chmod($folder, $mode);
+        }
+
+        return $return;
     }
-
-    // Reset permissions in reverse order.
-    foreach (array_reverse($reset_perms, TRUE) as $folder => $mode) {
-      chmod($folder, $mode);
-    }
-
-    return $return;
-  }
-
-
-
 }
--- src/PluginWrapper.php
+++ src/PluginWrapper.php
@@ -2,193 +2,198 @@
 
 /**
  * @file
- * Contains derhasi\Composer\Plugin.
+ * Contains deminy\Composer\Plugin.
  */
 
-namespace derhasi\Composer;
+namespace deminy\Composer;
 
 use Composer\Composer;
 use Composer\DependencyResolver\Operation\InstallOperation;
 use Composer\DependencyResolver\Operation\UninstallOperation;
 use Composer\DependencyResolver\Operation\UpdateOperation;
-use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
-use Composer\Plugin\PluginInterface;
+use Composer\Installer\InstallationManager;
 use Composer\Installer\PackageEvent;
-use Composer\Script\ScriptEvents;
+use Composer\Package\PackageInterface;
 use Composer\Util\Filesystem;
+use Exception;
 
 /**
  * Wrapper for making Plugin debuggable.
  */
-class PluginWrapper {
+class PluginWrapper
+{
+    /**
+     * @var IOInterface
+     */
+    protected $io;
 
-  /**
-   * @var \Composer\IO\IOInterface
-   */
-  protected $io;
+    /**
+     * @var Composer
+     */
+    protected $composer;
 
-  /**
-   * @var \Composer\Composer
-   */
-  protected $composer;
+    /**
+     * @var Filesystem
+     */
+    protected $filesystem;
 
-  /**
-   * @var \Composer\Util\Filesystem
-   */
-  protected $filesystem;
+    /**
+     * @var PathPreserver[string]
+     */
+    protected $preservers;
 
-  /**
-   * @var \derhasi\Composer\PathPreserver[string]
-   */
-  protected $preservers;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(Composer $composer, IOInterface $io) {
-    $this->io = $io;
-    $this->composer = $composer;
-    $this->filesystem = new Filesystem();
-  }
-
-  /**
-   * Pre Package event behaviour for backing up preserved paths.
-   *
-   * @param \Composer\Installer\PackageEvent $event
-   */
-  public function prePackage(PackageEvent $event) {
-
-    $packages = $this->getPackagesFromEvent($event);
-    $paths = $this->getInstallPathsFromPackages($packages);
-
-    $preserver = new PathPreserver(
-      $paths,
-      $this->getPreservePaths(),
-      $this->composer->getConfig()->get('cache-dir'),
-      $this->filesystem,
-      $this->io
-    );
-
-    // Store preserver for reuse in post package.
-    $this->preservers[$this->getUniqueNameFromPackages($packages)] = $preserver;
-
-    $preserver->preserve();
-  }
-
-  /**
-   * Pre Package event behaviour for backing up preserved paths.
-   *
-   * @param \Composer\Installer\PackageEvent $event
-   */
-  public function postPackage(PackageEvent $event) {
-    $packages = $this->getPackagesFromEvent($event);
-    $key = $this->getUniqueNameFromPackages($packages);
-    if ($this->preservers[$key]) {
-      $this->preservers[$key]->rollback();
-      unset($this->preservers[$key]);
-    }
-  }
-
-  /**
-   * Retrieves relevant package from the event.
-   *
-   * In the case of update, the target package is retrieved, as that will
-   * provide the path the package will be installed to.
-   *
-   * @param \Composer\Installer\PackageEvent $event
-   * @return \Composer\Package\PackageInterface[]
-   * @throws \Exception
-   */
-  protected function getPackagesFromEvent(PackageEvent $event) {
-
-    $operation = $event->getOperation();
-    if ($operation instanceof InstallOperation) {
-      $packages = array($operation->getPackage());
-    }
-    elseif ($operation instanceof UpdateOperation) {
-      $packages = array(
-        $operation->getInitialPackage(),
-        $operation->getTargetPackage(),
-      );
-    }
-    elseif ($operation instanceof UninstallOperation) {
-      $packages = array($operation->getPackage());
+    /**
+     * @inheritdoc
+     */
+    public function __construct(Composer $composer, IOInterface $io) 
+    {
+        $this->composer   = $composer;
+        $this->io         = $io;
+        $this->filesystem = new Filesystem();
     }
 
-    return $packages;
-  }
+    /**
+     * Pre Package event behaviour for backing up preserved paths.
+     *
+     * @param PackageEvent $event
+     */
+    public function prePackage(PackageEvent $event) 
+    {
 
-  /**
-   * Retrieve install paths from package installers.
-   *
-   * @param \Composer\Package\PackageInterface[] $packages
-   *
-   * @return string[]
-   */
-  protected function getInstallPathsFromPackages(array $packages) {
-    /** @var \Composer\Installer\InstallationManager $installationManager */
-    $installationManager = $this->composer->getInstallationManager();
+        $packages = $this->getPackagesFromEvent($event);
+        $paths = $this->getInstallPathsFromPackages($packages);
 
-    $paths = array();
-    foreach ($packages as $package) {
-      $paths[] = $installationManager->getInstallPath($package);
-    }
-    return $this->absolutePaths($paths);
-  }
+        $preserver = new PathPreserver(
+            $paths,
+            $this->getPreservePaths(),
+            $this->composer->getConfig()->get('cache-dir'),
+            $this->filesystem,
+            $this->io
+        );
 
-  /**
-   * Provides a unique string for a package combination.
-   *
-   * @param \Composer\Package\PackageInterface[] $packages
-   *
-   * @return string
-   */
-  protected function getUniqueNameFromPackages(array $packages) {
-    $return = array();
-    foreach ($packages as $package) {
-      $return[] = $package->getUniqueName();
-    }
-    sort($return);
-    return implode(', ', $return);
-  }
+        // Store preserver for reuse in post package.
+        $this->preservers[$this->getUniqueNameFromPackages($packages)] = $preserver;
 
-  /**
-   * Get preserve paths from root configuration.
-   *
-   * @return string[]
-   */
-  protected function getPreservePaths() {
-    $extra = $this->composer->getPackage()->getExtra();
-
-    if (!isset($extra['preserve-paths'])) {
-      $paths = $extra['preserve-paths'];
-    }
-    elseif (!is_array($extra['preserve-paths']) && !is_object($extra['preserve-paths'])) {
-      $paths = array($extra['preserve-paths']);
-    }
-    else {
-      $paths = array_values((array) $extra['preserve-paths']);
+        $preserver->preserve();
     }
 
-    return $this->absolutePaths($paths);
-  }
-
-  /**
-   * Helper to convert relative paths to absolute ones.
-   *
-   * @param string[] $paths
-   * @return string[]
-   */
-  protected function absolutePaths($paths) {
-    $return = array();
-    foreach ($paths as $path) {
-
-      if (!$this->filesystem->isAbsolutePath($path)) {
-        $path = getcwd() . '/' . $path;
-      }
-      $return[] = $path;
+    /**
+     * Pre Package event behaviour for backing up preserved paths.
+     *
+     * @param PackageEvent $event
+     */
+    public function postPackage(PackageEvent $event) 
+    {
+        $packages = $this->getPackagesFromEvent($event);
+        $key = $this->getUniqueNameFromPackages($packages);
+        if ($this->preservers[$key]) {
+            $this->preservers[$key]->rollback();
+            unset($this->preservers[$key]);
+        }
     }
-    return $return;
-  }
+
+    /**
+     * Retrieves relevant package from the event.
+     *
+     * In the case of update, the target package is retrieved, as that will
+     * provide the path the package will be installed to.
+     *
+     * @param PackageEvent $event
+     * @return PackageInterface[]
+     * @throws Exception
+     */
+    protected function getPackagesFromEvent(PackageEvent $event) 
+    {
+
+        $operation = $event->getOperation();
+        if ($operation instanceof InstallOperation) {
+            $packages = array($operation->getPackage());
+        } elseif ($operation instanceof UpdateOperation) {
+            $packages = array(
+            $operation->getInitialPackage(),
+            $operation->getTargetPackage(),
+            );
+        } elseif ($operation instanceof UninstallOperation) {
+            $packages = array($operation->getPackage());
+        }
+
+        return $packages;
+    }
+
+    /**
+     * Retrieve install paths from package installers.
+     *
+     * @param PackageInterface[] $packages
+     *
+     * @return string[]
+     */
+    protected function getInstallPathsFromPackages(array $packages) 
+    {
+        /** @var InstallationManager $installationManager */
+        $installationManager = $this->composer->getInstallationManager();
+
+        $paths = array();
+        foreach ($packages as $package) {
+            $paths[] = $installationManager->getInstallPath($package);
+        }
+
+        return $this->absolutePaths($paths);
+    }
+
+    /**
+     * Provides a unique string for a package combination.
+     *
+     * @param \Composer\Package\PackageInterface[] $packages
+     *
+     * @return string
+     */
+    protected function getUniqueNameFromPackages(array $packages) 
+    {
+        $return = array();
+        foreach ($packages as $package) {
+            $return[] = $package->getUniqueName();
+        }
+        sort($return);
+        return implode(', ', $return);
+    }
+
+    /**
+     * Get preserve paths from root configuration.
+     *
+     * @return string[]
+     */
+    protected function getPreservePaths() 
+    {
+        $extra = $this->composer->getPackage()->getExtra();
+
+        if (!isset($extra['preserve-paths'])) {
+            $paths = $extra['preserve-paths'];
+        } elseif (!is_array($extra['preserve-paths']) && !is_object($extra['preserve-paths'])) {
+            $paths = array($extra['preserve-paths']);
+        } else {
+            $paths = array_values((array) $extra['preserve-paths']);
+        }
+
+        return $this->absolutePaths($paths);
+    }
+
+    /**
+     * Helper to convert relative paths to absolute ones.
+     *
+     * @param string[] $paths
+     * @return string[]
+     */
+    protected function absolutePaths($paths) 
+    {
+        $return = array();
+        foreach ($paths as $path) {
+            if (!$this->filesystem->isAbsolutePath($path)) {
+                $path = getcwd() . '/' . $path;
+            }
+            $return[] = $path;
+        }
+
+        return $return;
+    }
 }
