12a0783459f1c57902551d9b621f3127b7872b9a
hw <hussainweb@gmail.com>
2020-09-12 16:27:01 -0400

    Fix phpcs issues

--- src/Plugin.php
+++ src/Plugin.php
@@ -72,10 +72,14 @@ class Plugin implements PluginInterface, EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public function deactivate(Composer $composer, IOInterface $io) {}
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
 
     /**
      * {@inheritdoc}
      */
-    public function uninstall(Composer $composer, IOInterface $io) {}
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
